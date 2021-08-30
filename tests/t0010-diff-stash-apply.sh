fd3420eff655756bc5a97c75bbfc68a6fbf4a9d4
Magnus Templing <magnus.templing@ericsson.com>
2021-08-30 09:21:19 +0200

    stash: use push over save
    save is deprecated since Git 2.15/2.16

--- tests/t0010-diff-stash-apply.sh
+++ tests/t0010-diff-stash-apply.sh
@@ -5,7 +5,7 @@ set -e
 function cleanup {
 	[ -e a.diff ] && rm a.diff
 
-	ove stash
+	ove stash push
 	ove stash drop
 
 	return 0
@@ -17,6 +17,6 @@ while read -r f; do
 	echo 123 >> "$f"
 done < <(find git-1 git-3 git-5 -path '*/.git' -prune -o -type f -print)
 ove diff > a.diff
-ove stash
+ove stash push
 ove apply a.diff
 ove stash drop
